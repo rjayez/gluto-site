f12d71e4eb6a951859f206dd13f1859f24e4a657
Romain J <romanusjazz@gmail.com>
2021-12-03 21:25:38 +0100

    feat: ajout lien réseaux sociaux

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -16,8 +16,15 @@
     <div class="my-4 mr-3 flex justify-center">
       <img src="../assets/logo.png" height="200" width="200" alt="Logo du streamer Tetryl"/>
     </div>
+    <div class="flex justify-evenly mx-8 gap-4">
+      <a class="inline" href="https://www.twitch.tv/letetryl"><img class="h-5" src="../assets/rs-twitch.png" alt="icone twitch" /></a>
+      <a href="https://www.youtube.com/user/DilanNoKaze"><img class="h-5" src="../assets/rs-youtube.png" alt="icone youtube"/></a>
+      <a href="https://twitter.com/LeTetryl"><img class="h-5" src="../assets/rs-twitter.png" alt="icone twitter"/></a>
+      <a href="https://www.instagram.com/tetryl_"><img class="h-5" src="../assets/rs-insta.png" alt="icone instagram"/></a>
+      <a href="https://discord.gg/K22x6Qkwwu"><img class="h-5" src="../assets/rs-discord.png" alt="icone discord"/></a>
+    </div>
 
-    <nav class="pl-3">
+    <nav class="pl-3 mt-8">
       <div class="py-2 flex cursor-pointer group" @click="go('')">
         <div class="flex justify-center w-1/5 pr-3 h-6">
           <img src="../assets/btn-qui-suisje.png" alt="" class="box-border h-6"/>
