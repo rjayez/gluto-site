0ab7be7b0d75fe347b122dd9f96f0f98e4f738e1
LeTetryl <michael.dognon@gmail.com>
2021-12-06 17:59:16 -0500

    Animation V2

--- plugin/script.js
+++ plugin/script.js
@@ -4,34 +4,42 @@ let ff7anim = anime.timeline();
 let valeurcarte = anime.timeline();
 let cardCount = document.querySelectorAll(".achievement-card-box").length;
 
+var myAudio = new Audio('card-sound.wav');
+
+
 ff7anim
-  .add({
-    targets: ".premier",
-    translateY: [150, 0],
-    transformOrigin: ["50% 100%", "50% 100%"],
-    easing: "easeOutQuad",
-    scale: [0, 0.8],
-    delay: 1000,
-    duration: 1000,
-    opacity: 1,
-  })
-  .add({
-    targets: ".premier",
-    transformOrigin: ["50% 100%", "50% 100%"],
-    easing: "easeOutQuad",
-    scale: [0.8, 0],
-    delay: 1000,
-    duration: 1000,
-    opacity: [1, 0],
-    // easing: 'easeInOutExpo'
-  })
+  // .add({
+  //   targets: ".premier",
+  //   translateY: [150, 0],
+  //   transformOrigin: ["50% 100%", "50% 100%"],
+  //   easing: "easeOutQuad",
+  //   scale: [0, 0.8],
+  //   delay: 1000,
+  //   duration: 1000,
+  //   opacity: 1,
+  // })
+  // .add({
+  //   targets: ".premier",
+  //   transformOrigin: ["50% 100%", "50% 100%"],
+  //   easing: "easeOutQuad",
+  //   scale: [0.8, 0],
+  //   delay: 1000,
+  //   duration: 1000,
+  //   opacity: [1, 0],
+  //   // easing: 'easeInOutExpo'
+  // })
   .add({
     targets: ".second",
     translateY: [150, 0],
     transformOrigin: ["50% 100%", "50% 100%"],
-    easing: "easeOutQuad",
+    specialEasing: {
+      width: "linear",
+      height: "easeInBounce"
+    },
+  
+    
     scale: [0, 0.8],
-    delay: 100,
+    delay: 3000,
     opacity: 1,
     // easing: 'easeInOutExpo'
   });
@@ -64,14 +72,14 @@ cardAnim
     duration: 400,
     offset: "-=400",
   })
-  .add({
-    targets: "#achievement-button",
-    opacity: 1,
-    bottom: "-75px",
-    easing: "easeOutCirc",
-    duration: 400,
-    offset: "-=200",
-  })
+  // .add({
+  //   targets: "#achievement-button",
+  //   opacity: 1,
+  //   bottom: "-75px",
+  //   easing: "easeOutCirc",
+  //   duration: 400,
+  //   offset: "-=200",
+  // })
 
   .add({
     targets: ".achievement-card-box",
@@ -84,12 +92,32 @@ cardAnim
     },
     complete: function () {
       animationDone = true;
-      // pulse();
+       pulse();
+       
+      myAudio.play();
     },
     elasticity: 100,
     duration: 800,
   });
 
+  function pulse() {
+    cardAnim = anime({
+      targets: ".achievement-card-box",
+      scale: [1, 0.95],
+      rotateZ: function (el, i) {
+        return [
+          Math.floor(cardCount / 2) * -15 + i * 15 + "deg",
+          Math.floor(cardCount / 2) * -17 + i * 17 + "deg",
+        ];
+      },
+      translateX: function (el, i) {
+        return Math.floor(cardCount / 2) * -112 + i * 112 + "px";
+      },
+      easing: "easeInOutCubic",
+      duration: 5000,
+      loop: true,
+      direction: "alternate",
+    });
 
   valeurcarte.add({
     targets: ".rare",
@@ -98,32 +126,17 @@ cardAnim
     loop: true,
   })
 
-  .add({
-    targets: ".legendaire",
-    boxShadow: ["0 0 0px rgba(255,184,67,0)", "0 0 40px rgba(255,184,67,1)"],
-    easing: "easeInOutCubic",
-    loop: true,
-  })
+  // .add({
+  //   targets: ".legendaire",
+  //   boxShadow: ["0 0 0px rgba(255,184,67,0)", "0 0 40px rgba(255,184,67,1)"],
+  //   easing: "easeInOutCubic",
+  //   easing:""
+  //   loop: true,
+  // })
 
   
 
-function pulse() {
-  cardAnim = anime({
-    targets: ".achievement-card-box",
-    scale: [1, 0.95],
-    rotateZ: function (el, i) {
-      return [
-        Math.floor(cardCount / 2) * -15 + i * 15 + "deg",
-        Math.floor(cardCount / 2) * -17 + i * 17 + "deg",
-      ];
-    },
-    translateX: function (el, i) {
-      return Math.floor(cardCount / 2) * -112 + i * 112 + "px";
-    },
-    easing: "easeInOutCubic",
-    duration: 5000,
-    loop: true,
-    direction: "alternate",
-  });
+
+
 }
 
