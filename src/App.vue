cc8071aec97c5860943b69fc67741be4315ab91c
Romain <romain.jayez@gmail.com>
2022-01-30 23:25:27 +0100

    fix: modif CSS

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="h-full">
     <SideMenu />
     <!--  Div avec une marge de la largeur du side menu pour compenser la fixed position -->
-    <div class="lg:ml-64">
-      <component id="page-component" class="mx-2 flex-auto pt-6 md:mx-8 lg:mx-4" :is="CurrentComponent" />
+    <div class="mx-2 flex-auto pt-6 md:mx-8 lg:mx-4 lg:ml-64">
+      <component id="page-component" :is="CurrentComponent" />
     </div>
     <LiveNotification />
   </div>
